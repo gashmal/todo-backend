560e9758645ec48c99cfeab368df796d15144df6
gashmal <tovonandroy@gmail.com>
2018-11-26 22:17:57 +0100

    adding search filter

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ app.use("/", cors());
 
 // CONNEXION AU SERVEUR
 mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://localhost:27017/todo",
-	{
+	process.env.MONGODB_URI || "mongodb://localhost:27017/todo", {
 		useNewUrlParser: true
 	},
-	function(err) {
+	function (err) {
 		if (err) console.error("Could not connect to mongodb.");
 	}
 );
@@ -31,68 +30,66 @@ const taskModel = mongoose.model("task", {
 // Routes
 // / en GET, pour que le client récupère la liste des tâches
 
-app.get("/", function(req, res) {
-	taskModel.find().exec(function(err, tasks) {
-		err ? res.json(err) : res.json(tasks);
+app.get("/", function (req, res) {
+	const filter = {};
+	if (req.query.description) {
+		filter.description = {
+			$regex: req.query.description,
+			$options: "i"
+		};
+	}
+
+	taskModel.find(filter).exec(function (err, tasks) {
+		err ? res.state(400).json(err) : res.json(tasks);
 	});
 });
 
 // /create en POST, pour que le client puisse créer une tâche
 
-app.post("/create", function(req, res) {
+app.post("/create", function (req, res) {
 	console.log(req.body);
 
 	const singleTask = new taskModel({
 		description: req.body.description,
 		finished: false
 	});
-	singleTask.save(function(err, obj) {
-		if (err) {
-			res.state(400).send("tfouuu");
-		} else {
-			res.json(obj);
-		}
+	singleTask.save(function (err, obj) {
+		err ? res.state(400).send("tfouuu") : res.json(obj);
 	});
 });
 
 // /update en POST, pour que le client puisse rendre une tâche faite ou non-faite
 
-app.post("/update/:id", function(req, res) {
-	taskModel.findOneAndUpdate(
-		{
+app.post("/update/:id", function (req, res) {
+	taskModel.findOneAndUpdate({
 			_id: req.params.id
-		},
-		{ $set: { finished: req.body.finished } },
-		function(err, task) {
-			if (!err) {
-				res.json(task);
-			} else {
-				res.send("aie aie aie");
+		}, {
+			$set: {
+				finished: req.body.finished
 			}
+		},
+		function (err, task) {
+			err ? res.state(400).send("aie aie aie") : res.json(task);
 		}
+
 	);
 });
 
 // /delete en POST, pour que le client puisse supprimer une tâche
 
-app.post("/delete/:id", function(req, res) {
-	taskModel.findOneAndDelete(
-		{
+app.post("/delete/:id", function (req, res) {
+	taskModel.findOneAndDelete({
 			_id: req.params.id
 		},
-		function(err, task) {
-			if (!err) {
-				res.send("deleted");
-			} else {
-				res.send("aie aie aie");
-			}
+		function (err, task) {
+			err ? res.state(400).send("aie aie aie") : res.send("deleted");
 		}
 	);
 });
 
 // Toutes les méthodes HTTP (GET, POST, etc.) des pages non trouvées afficheront
 // une erreur 404
-app.all("*", function(req, res) {
+app.all("*", function (req, res) {
 	res.status(404).json({
 		error: "Not Found"
 	});
@@ -100,6 +97,6 @@ app.all("*", function(req, res) {
 
 // Initialisation du serveur
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
 	console.log("Serveur initialisé");
 });
